e458f3c1eed3ecdbe3193f1628d823e54638ca0d
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2019-11-07 14:10:57 -0500

    Usar la carpeta home referenciada <~>

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-DIR=/home/carper/local/utils/ox/openxava-6.2/workspace
+DIR=~/local/utils/ox/openxava-6.2/workspace
 DIR1=$DIR/Ingenio/web/WEB-INF
 DIR2=$DIR/OpenXava/lib
 java -cp $DIR1/classes:$DIR1/lib/*:$DIR2/* _run._Run_Ingenio
