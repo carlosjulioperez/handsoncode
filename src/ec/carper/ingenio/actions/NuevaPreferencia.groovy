c67b2a409ad52469f606c4349969d68fa52ab04d
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2019-11-28 13:08:41 -0500

    Ejemplo de acciones para validar que exista un solo registro en un módulo

--- src/ec/carper/ingenio/actions/NuevaPreferencia.groovy
+++ src/ec/carper/ingenio/actions/NuevaPreferencia.groovy
@@ -1,5 +1,32 @@
 package ec.carper.ingenio.actions
 
+import javax.persistence.Query;
 import org.openxava.actions.*
 import org.openxava.jpa.*
 import ec.carper.ingenio.model.Preferencias
+import static org.openxava.jpa.XPersistence.*;
+
+class NuevaPreferencia extends ViewBaseAction implements IChainAction{
+
+    private String nextAction = null // Para guardar la siguiente acción a ejecutar
+
+    void execute() throws Exception{
+        //TODO Ejemplo para ejecutar queries desde Groovy
+        Query query = getManager().createQuery("select count(*) from Preferencias")
+        def numero = (Integer)query.getSingleResult()
+        //System.out.println(numero)
+
+        if ( numero >0 ){
+            addMessage ("msgSoloUnRegistro")
+            
+            resetDescriptionsCache()
+            getView().clear()
+        }else 
+            nextAction = "CRUD.new"
+    }
+    
+    // Obligatorio por causa de 'IChainAction'
+    String getNextAction() throws Exception {
+        return nextAction // Si es nulo no se encadena con ninguna acción
+    }
+}
