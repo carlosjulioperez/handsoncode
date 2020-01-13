9ba6dbf4941031b57b41d1e5127f9dd6fa4db880
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2020-01-13 14:27:24 -0500

    Cálculos de promedios en los jugos

--- etc/groovy/GroovyConsole.groovy
+++ etc/groovy/GroovyConsole.groovy
@@ -4,3 +4,12 @@ timeList << "B"
 timeList << "C"
 
 timeList.each {println it}
+
+
+// https://stackoverflow.com/questions/4560546/how-do-i-round-a-number-in-groovy    
+def valor = 1.255 / 0.12
+println valor
+println Math.round(valor * 100) / 100
+
+println ( new BigDecimal(1.2686).setScale(2, BigDecimal.ROUND_HALF_UP))
+
--- src/ec/carper/ingenio/model/Jugo.groovy
+++ src/ec/carper/ingenio/model/Jugo.groovy
@@ -8,7 +8,16 @@ import org.openxava.calculators.*
 import org.openxava.model.*
 
 @Entity
-// @Tab(properties="""fecha""")
+@Tab(properties="""
+    fecha,
+    avgJeBri, avgJePol, avgJeSac, avgJePur,
+    avgJdBri, avgJdPol, avgJdSac, avgJdPur,
+    avgJcBri, avgJcPol, avgJcSac, avgJcPur,
+    avgJnBri, avgJnPol, avgJnSac, avgJnPur,
+    avgJrBri, avgJrPol, avgJrSac, avgJrPur,
+    avgJfBri, avgJfPol, avgJfSac, avgJfPur
+""")
+@View(members=  """fecha;detalles""")
 class Jugo extends Identifiable{
 
     @Version
@@ -29,4 +38,96 @@ class Jugo extends Identifiable{
         jfBri,jfPol,calJfSac,calJfPur
     """)
     Collection<JugoDetalle>detalles
+
+    BigDecimal avgJeBri
+    BigDecimal avgJePol
+    BigDecimal avgJeSac
+    BigDecimal avgJePur
+
+    BigDecimal avgJdBri
+    BigDecimal avgJdPol
+    BigDecimal avgJdSac
+    BigDecimal avgJdPur
+
+    BigDecimal avgJcBri
+    BigDecimal avgJcPol
+    BigDecimal avgJcSac
+    BigDecimal avgJcPur
+
+    BigDecimal avgJnBri
+    BigDecimal avgJnPol
+    BigDecimal avgJnSac
+    BigDecimal avgJnPur
+
+    BigDecimal avgJrBri
+    BigDecimal avgJrPol
+    BigDecimal avgJrSac
+    BigDecimal avgJrPur
+
+    BigDecimal avgJfBri
+    BigDecimal avgJfPol
+    BigDecimal avgJfSac
+    BigDecimal avgJfPur
+
+    BigDecimal getAvg(String propiedad){
+        def lista = []
+        detalles.each {
+            //def valor = (BigDecimal)Eval.x(it, "x.jeBri")
+            def valor = (BigDecimal)Eval.x(it, "x."+propiedad)
+            if (valor > 0) lista << valor
+        }
+        return lista.size()>0 ? ( lista.sum() / lista.size() ).setScale(2, BigDecimal.ROUND_HALF_UP) : 0
+    }
+
+
+    @PrePersist // Ejecutado justo antes de grabar el objeto por primera vez
+    private void preGrabar() throws Exception {
+        sincronizarPropiedadesPersistentes()
+    }
+    
+    @PreUpdate
+    void sincronizarPropiedadesPersistentes(){
+        setAvgJeBri(getAvg("jeBri"))
+        setAvgJePol(getAvg("jePol"))
+        setAvgJeSac(getAvg("calJeSac"))
+        setAvgJePur(getAvg("calJePur"))
+
+        setAvgJdBri(getAvg("jdBri"))
+        setAvgJdPol(getAvg("jdPol"))
+        setAvgJdSac(getAvg("calJdSac"))
+        setAvgJdPur(getAvg("calJdPur"))
+
+        setAvgJcBri(getAvg("jcBri"))
+        setAvgJcPol(getAvg("jcPol"))
+        setAvgJcSac(getAvg("calJcSac"))
+        setAvgJcPur(getAvg("calJcPur"))
+
+        setAvgJnBri(getAvg("jnBri"))
+        setAvgJnPol(getAvg("jnPol"))
+        setAvgJnSac(getAvg("calJnSac"))
+        setAvgJnPur(getAvg("calJnPur"))
+
+        setAvgJrBri(getAvg("jrBri"))
+        setAvgJrPol(getAvg("jrPol"))
+        setAvgJrSac(getAvg("calJrSac"))
+        setAvgJrPur(getAvg("calJrPur"))
+
+        setAvgJfBri(getAvg("jfBri"))
+        setAvgJfPol(getAvg("jfPol"))
+        setAvgJfSac(getAvg("calJfSac"))
+        setAvgJfPur(getAvg("calJfPur"))
+    }
+
+    // @PrePersist // Ejecutado justo antes de grabar el objeto por primera vez
+    // private void preGrabar() throws Exception {
+    //     recalcularAvgJeBri()
+    // }
+    //
+    // @PreUpdate
+    // void recalcularAvgJeBri() {
+    //     def val = getAvg("jeBri")
+    //     println ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + val
+    //     //setAvgJeBri(getAvg("jeBri"))
+    //     setAvgJeBri(val)
+    // }
 }
