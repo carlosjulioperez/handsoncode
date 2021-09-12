46829b8647a0a0e9c3cea29226dff864a979d932
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2021-09-12 00:24:42 -0500

    Comparación de nim y go en RPI4

---
+++ go/fibonacci.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "fmt"
+)
+
+func FibonacciRecursion(n int) int {
+  if n <= 1 {
+    return n
+  }
+  return FibonacciRecursion(n-1) + FibonacciRecursion(n-2)
+}
+
+func main() {
+  fmt.Print( FibonacciRecursion(40) )
+}
