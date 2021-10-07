12254ebcea894831d05c739629cb7f9a3c3bab4e
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2021-10-06 23:55:53 -0500

    User and Phone identities

---
+++ java/springBoot/users/src/main/java/ec/carper/users/data/model/Phone.java
@@ -0,0 +1,28 @@
+package ec.carper.users.data.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@Getter @Setter @ToString
+public class Phone {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private Long number;
+    
+    private Long citycode;
+    
+    private Long countrycode;
+    
+}
---
+++ java/springBoot/users/src/main/java/ec/carper/users/data/model/User.java
@@ -0,0 +1,27 @@
+package ec.carper.users.data.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@Getter @Setter @ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+    
+    private String password;
+    
+}
