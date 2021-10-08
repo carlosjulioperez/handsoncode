1490e912b110b178a08bf67679c61476bb7ba39a
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2021-10-08 01:59:06 -0500

    Agregar servicios y web y DTO

--- java/springBoot/users/src/main/java/ec/carper/users/web/UserController.java
+++ java/springBoot/users/src/main/java/ec/carper/users/web/UserController.java
@@ -1,10 +1,66 @@
 package ec.carper.users.web;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ec.carper.users.data.model.User;
+import ec.carper.users.data.payload.request.UserRequest;
+import ec.carper.users.data.payload.response.MessageResponse;
+import ec.carper.users.service.UserService;
+import io.swagger.annotations.ApiResponses;
+
+// https://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
 @RestController
 @RequestMapping("/user")
+@ApiResponses(value = {
+    @io.swagger.annotations.ApiResponse(code = 400, message = "This is a bad request, please follow the API documentation for the proper request format"),
+    @io.swagger.annotations.ApiResponse(code = 401, message = "Due to security constraints, your access request cannot be authorized"),
+    @io.swagger.annotations.ApiResponse(code = 500, message = "The server is down. Please bear with us."),
+})
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.getAllUser();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+        User user = userService.getAsSinglUser(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
     
+    @PostMapping("/add")
+    public ResponseEntity<MessageResponse> addUser( @RequestBody UserRequest userRequest){
+        MessageResponse newUser = userService.createUser(userRequest);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{id}")
+    public Optional<User> updateUser( @PathVariable Long id, @RequestBody UserRequest userRequest){
+        return userService.updateUser(id, userRequest);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable("id") Long id){
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+ 
 }
