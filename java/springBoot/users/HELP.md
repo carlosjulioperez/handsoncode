347958c2b443067f074c0090cc81d05aaeaedc4f
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2021-10-13 14:11:49 -0500

    Generating fat jar

--- java/springBoot/users/HELP.md
+++ java/springBoot/users/HELP.md
@@ -1,7 +1,13 @@
 # Project User
 
+# Generating fatJar
+$ ./gradlew uberJar
+
 ## Starting service
 ./gradlew bootRun
+
+## Packaging app
+./gradlew bootJar
 
 ## Running H2 Console
 `http://localhost:8080/h2`
--- java/springBoot/users/src/main/java/ec/carper/users/UsersApplication.java
+++ java/springBoot/users/src/main/java/ec/carper/users/UsersApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UsersApplication {
 
+	// Structure:
+	// https://medium.com/the-resonant-web/spring-boot-2-0-project-structure-and-best-practices-part-2-7137bdcba7d3
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
 	}
